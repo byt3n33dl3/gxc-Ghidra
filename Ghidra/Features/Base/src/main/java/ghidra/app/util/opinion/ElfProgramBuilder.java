9c39b8a7580ea274dba85cf646cb851cc945b63d
pxcs <sulabaziz09@gmail.com>
2025-02-17 23:11:13 +0800

    merge-hexxa origin vol 11.point

--- Ghidra/Features/Base/src/main/java/ghidra/app/util/opinion/ElfProgramBuilder.java
+++ Ghidra/Features/Base/src/main/java/ghidra/app/util/opinion/ElfProgramBuilder.java
@@ -1,18 +1,3 @@
-/* ###
- * IP: GHIDRA
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package ghidra.app.util.opinion;
 
 import java.io.*;
@@ -3581,7 +3566,7 @@ class ElfProgramBuilder extends MemorySectionResolver implements ElfLoadHelper {
 		SymbolTable symbolTable = program.getSymbolTable();
 		Symbol sym = symbolTable.createLabel(addr, name, namespace, SourceType.IMPORTED);
 		if (isPrimary) {
-			checkPrimary(sym);
+			sym = checkPrimary(sym);
 		}
 		if (pinAbsolute && !sym.isPinned()) {
 			sym.setPinned(true);
