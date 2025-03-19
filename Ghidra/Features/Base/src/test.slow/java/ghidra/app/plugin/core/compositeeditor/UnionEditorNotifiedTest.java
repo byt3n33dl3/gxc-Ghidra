979b4b5e8234e08f78d0e3b66fe7ea208b3fb155
Sulaiman <sulabaziz09@gmail.com>
2025-03-20 02:15:06 +0800

    remote-tracking branch vol ESci ferry
    'origin/patch'

--- Ghidra/Features/Base/src/test.slow/java/ghidra/app/plugin/core/compositeeditor/UnionEditorNotifiedTest.java
+++ Ghidra/Features/Base/src/test.slow/java/ghidra/app/plugin/core/compositeeditor/UnionEditorNotifiedTest.java
@@ -1,26 +1,8 @@
-/* ###
- * IP: GHIDRA
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
 package ghidra.app.plugin.core.compositeeditor;
 
 import static org.junit.Assert.*;
-
 import java.awt.Window;
-
 import javax.swing.SwingUtilities;
-
 import org.junit.*;
 
 import ghidra.program.model.data.*;
@@ -119,6 +101,14 @@ public class UnionEditorNotifiedTest extends AbstractUnionEditorTest {
 		});
 
 		waitForSwing();
+
+		// Verify the Close Union Editor? dialog is displayed.
+		Window dialog = waitForWindow("Close Union Editor?");
+		assertNotNull(dialog);
+		pressButtonByText(dialog, "No");
+		dialog.dispose();
+		dialog = null;
+
 		// refUnion* gets removed
 		assertEquals(num - 1, model.getNumComponents());
 		assertTrue(dt18.isEquivalent(getDataType(18)));
