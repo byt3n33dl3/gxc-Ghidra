eded7de8bf12786e4f955aa06c2f86b4234746a6
Sulaiman <sulabaziz09@gmail.com>
2025-04-06 01:50:48 +0800

    char/ filesystem ready for vol.12

--- gxc/tools/lib/filesystem.c
+++ gxc/tools/lib/filesystem.c
@@ -1,22 +1,3 @@
-/**
-  * GreenPois0n Cynanide - filesystem.c
-  * Copyright (C) 2010 Chronic-Dev Team
-  * Copyright (C) 2010 Joshua Hill
-  *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the GNU General Public License as published by
-  * the Free Software Foundation, either version 3 of the License, or
-  * (at your option) any later version.
-  *
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  * GNU General Public License for more details.
-  *
-  * You should have received a copy of the GNU General Public License
-  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- **/
-
 #include <stdio.h>
 
 #include "filesystem.h"
@@ -45,14 +26,12 @@ int fs_init() {
 		printf("Found fs_mount at 0x%x\n", fs_mount);
 	}
 
-	//fs_unmount = find_fs_unmount();
 	if(fs_unmount == NULL) {
 		puts("Unable to find fs_unmount\n");
 	} else {
 		printf("Found fs_unmount at 0x%x\n", fs_unmount);
 	}
 
-	//fs_load_file = find_fs_load_file();
 	if(fs_load_file == NULL) {
 		puts("Unable to find fs_load_file\n");
 	} else {
@@ -60,7 +39,7 @@ int fs_init() {
 	}
 
 	if(fs_mount && fs_unmount && fs_load_file) {
-		cmd_add("fs", &fs_cmd, "perform operations on the filesystem");
+		cmd_add("fs", &fs_cmd, "perform operations on the gxc extraction");
 	}
 
 	return 0;
@@ -69,6 +48,7 @@ int fs_init() {
 int fs_cmd(int argc, CmdArg* argv) {
 	char* path = NULL;
 	char* action = NULL;
+	char* solution = NULL;
 	char* device = NULL;
 	void* address = NULL;
 	unsigned int* size = 0;
@@ -95,7 +75,7 @@ int fs_cmd(int argc, CmdArg* argv) {
 			fs_mount(device, "hfs", path);
 
 		} else if(!strcmp(action, "load")) {
-			path = argv[2].string;
+			path = argv[1].string;
 			address = (void*) argv[3].uinteger;
 			fs_load_file(path, address, size);
 		}
